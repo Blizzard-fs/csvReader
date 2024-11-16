56a7f371d07031f0e4bce3afbaaa91ba4f70a017
Stefan Tomà <58548607+Blizzard-fs@users.noreply.github.com>
2024-11-16 18:58:51 +0100

    Changed script to class for easier implementation.

--- index.php
+++ index.php
@@ -1,31 +1,69 @@
 <?php
-error_reporting(E_ALL); ini_set('display_errors', 1);
-$separator      = ',';
-$pathToFile     = '';
-$csvFileName    = 'available_stock_en_all_sizes.csv';
-$csv = $fields = array(); $i = 0;
-$handle = @fopen($pathToFile . $csvFileName, "r");
-if ($handle) {
-    $bom = fread($handle, 3);
-    if ($bom != "\xEF\xBB\xBF")
-    {
-        rewind($handle);
+
+class CsvParser {
+    private $filePath;
+    private $separator;
+
+    public function __construct($filePath, $separator = ',') {
+        $this->filePath = $filePath;
+        $this->separator = $separator;
     }
-    while (($row = fgetcsv($handle, 0, $separator)) !== false) {
-        if (empty($fields)) {
-            $fields = $row;
-            continue;
+
+    public function parse() {
+        $this->validateFile();
+        $handle = $this->openFile();
+
+        $this->handleBom($handle);
+        $headers = $this->getHeaders($handle);
+        $data = $this->getData($handle, $headers);
+
+        fclose($handle);
+        return $data;
+    }
+
+    private function validateFile() {
+        if (!file_exists($this->filePath) || !is_readable($this->filePath)) {
+            throw new Exception("Error: Unable to read the file '{$this->filePath}'.");
         }
-        foreach ($row as $k=>$value) {
-            $csv[$i][$fields[$k]] = $value;
+    }
+
+    private function openFile() {
+        $handle = fopen($this->filePath, "r");
+        if (!$handle) {
+            throw new Exception("Error: Unable to open the file.");
         }
-        $i++;
+        return $handle;
     }
-    if (!feof($handle)) {
-        echo "Error: unexpected fgets() fail\n";
+
+    private function handleBom($handle) {
+        if (fread($handle, 3) !== "\xEF\xBB\xBF") {
+            rewind($handle);
+        }
     }
-    fclose($handle);
+
+    private function getHeaders($handle) {
+        $headers = fgetcsv($handle, 0, $this->separator);
+        if (!$headers) {
+            throw new Exception("Error: Failed to read headers from the file.");
+        }
+        return $headers;
+    }
+
+    private function getData($handle, $headers) {
+        $data = [];
+        while (($row = fgetcsv($handle, 0, $this->separator)) !== false) {
+            $data[] = array_combine($headers, $row);
+        }
+        return $data;
+    }
 }
-echo "<pre>";
-var_dump($csv);
-die();
+
+// Example Usage
+try {
+    $csvParser = new CsvParser('available_stock_en_all_sizes.csv');
+    $data = $csvParser->parse();
+    echo "<pre>";
+    print_r($data);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
